5c39208b0ea671a6a827d0034277c85952d1e625
Aleks <aleksandar.trpkovski@gmail.com>
2022-04-26 22:22:42 +1000

    Added configuration and Environmental variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `envs/${process.env.NODE_ENV}.env`,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
