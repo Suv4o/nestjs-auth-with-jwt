7fc3d89327c62410474b1c9d496ffb6bf59b7910
Aleks <aleksandar.trpkovski@gmail.com>
2022-05-01 16:36:43 +1000

    Update port to 3001

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ global['fetch'] = require('node-fetch');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
