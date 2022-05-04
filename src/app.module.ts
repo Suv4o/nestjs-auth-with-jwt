838f77142ba84b226a46a1967f6e79801de0e1c8
Aleks <aleksandar.trpkovski@gmail.com>
2022-05-04 17:45:25 +1000

    Removed the import Auth module in main app.module.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { typeOrmConfigAsync } from 'config/typeorm.config';
 import { AppController } from './app.controller';
@@ -14,7 +13,6 @@ import { AuthConfig } from 'config/auth.config';
       envFilePath: `env/${process.env.NODE_ENV}.env`,
     }),
     TypeOrmModule.forRootAsync(typeOrmConfigAsync),
-    AuthModule,
     UserModule,
   ],
   providers: [AuthConfig],
